241cad2abaaf3a703ef6fe8f50c8852398c69124
Antonioj562 <AntonioJLoyola@gmail.com>
2026-03-19 12:20:27 -0700

    Added github actions .yyml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
-
+const URL = 'https://antonioj562.github.io/portfolio-workshop'
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site : URL,
   integrations: [mdx()]
 });
